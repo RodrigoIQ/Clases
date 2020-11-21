a2fdd5edea54a0c2b0ff757bad6ec222ff920c13
RodrigoIQTablet <riq.dsing@gmail.com>
2020-11-21 16:28:58 -0600

    added graphing fo every step in the walk

--- Curso de Estadística Computacional con Python/drunkGuy.py
+++ Curso de Estadística Computacional con Python/drunkGuy.py
@@ -12,4 +12,10 @@ class TraditionalDrunk(DrunkGuy):
 
     def walkPatern(self):
         return random.choice([(0,1),(1,0),(0,-1),(-1,0)])
-        
+
+class crazyDrunk(DrunkGuy):
+    def __init__(self,name):
+        super().__init__(name)
+
+    def walkPatern(self):
+        return random.choice([(0,2),(3,0),(0,-2),(-2,0)])
--- Curso de Estadística Computacional con Python/random_walk.py
+++ Curso de Estadística Computacional con Python/random_walk.py
@@ -5,18 +5,34 @@ from drunkGuy import TraditionalDrunk
 from field import Field
 from location import Location
 from bokeh.plotting import figure, show
+import numpy as np
+
+steps_for_x=[]
+steps_for_y=[]
 
 def graphing(x,y):
-    graph = figure(title= "Random Walk",x_axis_label="Steps", y_axis_label="distance")
-    graph.line(x,y, legend="mid distance")
+    N = 100000
+    l = np.random.random(size=N) * 100
+    i = np.random.random(size=N) * 100
+    radii = np.random.random(size=N) * 1.5
+    colors = [
+    "#%02x%02x%02x" % (int(r), int(g), 150) for r, g in zip(50+2*l, 30+2*i)
+    ]
+    radii = np.random.random(size=N) * 1.5
+
+    graph = figure(title= "Random Walk",x_axis_label="x", y_axis_label="y")
+    graph.line(x,y, legend="does it look cool?")
 
     show(graph)    
 
 def walk(field,drunkGuy,steps):
     start = field.get_location(drunkGuy)
-
+    
     for _ in range(steps):
         field.move_drunk_guy(drunkGuy)
+        steps_for_x.append(field.get_location(drunkGuy).x)
+        steps_for_y.append(field.get_location(drunkGuy).y)
+
 
     return start.distance(field.get_location(drunkGuy))
 
@@ -36,6 +52,7 @@ def simulate_walk(steps,number_of_walks,type_of_drunkGuy):
 
 def main(steps_walked,number_of_walks,type_of_drunkGuy):
     mid_distance_per_walk=[]
+    
     for steps in steps_walked:
         distance = simulate_walk(steps,number_of_walks,type_of_drunkGuy)
         medium_distance = round(sum(distance)/len(distance),4)
@@ -46,11 +63,11 @@ def main(steps_walked,number_of_walks,type_of_drunkGuy):
         print(f'Medium distance = {medium_distance}')
         print(f'Max distance = {max_distance}')
         print(f'Min distance = {min_distance}')
-    graphing(steps_walked, mid_distance_per_walk)
+    graphing(steps_for_x,steps_for_y)
 
 
 if __name__ == '__main__':
-    steps_walked = [10, 1000, 10000]
-    number_of_walks = 100
+    steps_walked = [100000]
+    number_of_walks = 1
 
     main(steps_walked,number_of_walks,TraditionalDrunk) 
